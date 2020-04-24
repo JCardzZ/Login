1e0ddf78315efa71117324687823995623fed2fd
JCardzZ <cardozamanuel96@gmail.com>
2020-04-23 20:39:05 -0600

    Clase 23/04/2020

---
+++ Login/src/main/java/com/mm/com/loginsecurity/util/Passgenerator.java
@@ -0,0 +1,11 @@
+package com.mm.com.loginsecurity.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Passgenerator {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
+	System.out.println(bCryptPasswordEncoder.encode("123"));
+	}
+}
